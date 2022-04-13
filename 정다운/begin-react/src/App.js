0c281310f15b4bd704b3c310f03d5eea7048dd24
JeongDowny <daun5535@naver.com>
2022-04-14 07:48:51 +0900

    2022.04.14

---
+++ 정다운/begin-react/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
+
+  return (
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+  );
+}
+
+export default App;
--- 정다운/실습.js
+++ 정다운/실습.js
@@ -1,30 +1,4 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const getDog = async () => {
-  await sleep(1000);
-  return '멍멍이';
-};
-
-const getRabbit = async () => {
-  await sleep(500);
-  return '토끼';
-};
-const getTurtle = async () => {
-  await sleep(3000);
-  return '거북이';
-};
-
-async function process() {
-  const [dog, rabbit, turtle] = await Promise.all([
-    getDog(),
-    getRabbit(),
-    getTurtle()
-  ]);
-  console.log(dog);
-  console.log(rabbit);
-  console.log(turtle);
-}
-
-process();
+const animals = ['개', '고양이', '참새'];
+const anotherAnimals = [...animals, '비둘기'];
+console.log(animals); //[ '개', '고양이', '참새' ]
+console.log(anotherAnimals); // [ '개', '고양이', '참새', '비둘기' ]
