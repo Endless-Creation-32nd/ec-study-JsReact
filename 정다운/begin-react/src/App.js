376794653cd0cf537b9142525c4e58a3332b3a4d
JeongDowny <daun5535@naver.com>
2022-04-14 17:16:45 +0900

    2022.04.14

--- 정다운/begin-react/src/App.js
+++ 정다운/begin-react/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css';
+import Counter from './Counter';
 
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
-
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
-  );
+    <Counter />
+  )
 }
 
 export default App;
---
+++ 정다운/begin-react/src/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Counter() {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>+1</button>
+      <button>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
