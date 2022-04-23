ad329fe78444bf001c98a55652eb189bd0ed550c
JeongDowny <daun5535@naver.com>
2022-04-23 22:25:05 +0900

    2022.04.23

--- 정다운/begin-react/src/Counter.js
+++ 정다운/begin-react/src/Counter.js
@@ -4,11 +4,11 @@ function Counter() {
   const [number, setNumber] = useState(0);
 
   const onIncrease = () => {
-    setNumber(number + 1)
+    setNumber(prevNumber => prevNumber + 1);
   }
 
   const onDecrease = () => {
-    setNumber(number - 1)
+    setNumber(prevNumber => prevNumber - 1);
   }
 
   return (
@@ -17,7 +17,7 @@ function Counter() {
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
-  )
+  );
 }
 
 export default Counter;
